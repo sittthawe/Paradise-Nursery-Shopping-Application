192cbb414e7c7ccc350db8151a0493182351a2bd
sittthawe <128010151+sittthawe@users.noreply.github.com>
2026-02-14 12:23:49 +0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="landing-page">
+      <h1>Paradise Nursery</h1>
+      <p>Bringing Nature Closer to You</p>
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
